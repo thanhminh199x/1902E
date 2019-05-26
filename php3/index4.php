32388a825c8398318df538c6d976ca19915da6f6
dell <chuthanhminh9x@gmail.com>
2019-05-26 20:32:26 +0700

    ss
    sss

---
+++ php3/index4.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Vòng lặp while
+    Cú pháp :
+    điểm khởi đầu của vòng lặp
+    while(điều kiện chạy của vòng lặp) {
+        // code thực thi bên trong vòng lặp
+
+
+        // cuối vòng lặp
+        sự thay đổi của biến đếm sau mỗi lần chạy
+    }
+</pre>
+<?php
+echo "<br>In ra các từ 0 đến 19";
+$i = 0;
+while ($i <20){
+    echo "<br> $i";
+    $i++;
+
+}
+echo "<br>In ra các chẵn từ o đến 19";
+$i = 0
+while ($i<20){
+    echo "<br> $i";
+    $i += 2;
+
+}
+echo "<br> In ra các cẵn từ 0 đến 19";
+$i = 0
+while($i <20){
+    if (($i%2) == 0){
+        echo "<be> $i";
+    }
+    $i ++;
+}
+?>
+</body>
+</html>
